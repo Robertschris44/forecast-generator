31cc3c098dd9313adfc0d0996e7226c539fc1bf6
Chris Key <robertschris010@gmail.com>
2022-07-30 23:08:28 -0500

    added searchbtn

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,13 @@ var searchBtn = $("#search-btn");
 var periodicDivs = [$("#periodic-1div"), ("#periodic-2div"), ("#periodic-3div"), ("#periodic-4div"), ("#periodic-5div")];
 var nameCity = " ";
 var todayDate = moment().format("M/D/YYYY");
+
+var storedCities = JSON.parse (localStorage.getItem("storedCites")) || [];
+//create for loop and list
+for (var i = 0; i < storedCities.lenght; i ++) {
+    var city = savedCities[i];
+    var nameCityEl = $ ("<li>");
+    nameCityEl.addClass("btn list-group-item");
+    nameCityEl.text(city);
+    $("#list-city").append(nameCityEl);
+}
